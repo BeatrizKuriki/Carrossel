681ffc2c7d707b077fec0fc36a6373dc5a3589b4
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-11-05 09:05:13 -0300

    almost done with the img

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,41 @@
-const imgs = document.getElementById('imgs');
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
 
-const img = document.querySelectorAll('#imgs img');
+const img = document.querySelectorAll('#imgs img')
 
-let idx = 0;
+let idx = 0
 
-let interval = setInterval(run, 2000);
+let interval = setInterval(run, 2000)
 
-function run(){
+function run() {
     idx++
-    changeImage();
+    changeImage()
 }
 
-function changeImage(){
-    if(idx > img.length -1){
+function changeImage() {
+    if(idx > img.length - 1) {
         idx = 0
-    }else if(idx < 0 ){
-        idx = img.length -1
+    } else if(idx < 0) {
+        idx = img.length - 1
     }
 
     imgs.style.transform = `translateX(${-idx * 500}px)`
 }
 
-rightBtn.addEventListener('click', ()=>{
-    idd--;
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
 
-    changeImage();
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
 })
 
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
